92b0dbeeae73ff0e318066a6801d24a3587c11db
soph <sophia@javamate.net>
2025-09-27 20:48:05 -0400

    Completed

--- src/HighorLow.java
+++ src/HighorLow.java
@@ -1,58 +1,57 @@
 import java.util.Scanner;
 import java.util.Random;
 
-public class HighorLow {
-    public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
-    Random Randy = new Random();
-    int randomValue = Randy.nextInt(10) + 1;
-    int userValue = 0;
-    boolean done = false;
-    System.out.println("Enter a number [1-10]: ");
-    do
+public class HighorLow
+{
+    public static void main(String[] args)
     {
-        if (in.hasNextInt())
+        Scanner in = new Scanner(System.in); // names the scanner
+        Random Randy = new Random(); // names the randomizer
+        int randomValue = Randy.nextInt(10) + 1; // gives a boundary for the randomizer to randomize within
+        boolean done = false; // sets the done value to false
+
+
+        do // all of this should be done WHILE done does NOT equal true...while user is beginning or has entered the wrong value
         {
-            userValue = in.nextInt();
-            if (userValue >= 1 && userValue <= 10)
+            System.out.println("Enter a number [1-10]: "); // collects the initial input
+            if (in.hasNextInt()) // the following should happen if the input is an integer value
             {
-                System.out.println("The number is " + randomValue + ".");
-                if(userValue == randomValue)
+                int userValue = in.nextInt(); // puts the input into an integer, "userValue"
+                in.nextLine();
+                if (userValue >= 1 && userValue <= 10) // checking to see if the integer is between 1 and 10 inclusive
                 {
-                    System.out.println("You're right on the money!");
+                    done = true; // user entered a good input, they've done their job, done gets set to true.
+                    System.out.println("The number is " + randomValue + "."); // tells the user what the random number is
+                    if(userValue == randomValue) // checks for an exact match
+                    {
+                        System.out.println("You're right on the money!"); // tells the user they guessed correctly
+                    }
+                    else if(userValue > randomValue) // checks for a higher value
+                    {
+                        System.out.println("You guessed too high.");
+                    }
+                    else // if their guess is lower than the random value
+                    {
+                        System.out.println("You guessed too low.");
+                    }
                 }
-                else if(userValue > randomValue)
+                else // if it's an integer outside of 1 and 10 inclusive
                 {
-                    System.out.println("You guessed too high.");
+                    int trash = in.nextInt();
+                    in.nextLine();
+                    System.out.println("You entered an incorrect value, '" + trash + "'. You must enter a number between 1 and 10 inclusive.");
                 }
-                else // if their guess is lower than the random value
-                {
-                    System.out.println("You guessed too low.");
-                }
-
-
-                done = true;
+            }
+            else if (in.hasNextDouble())
+            {
+                double doubleTrash = in.nextDouble();
+                System.out.println("You entered an incorrect value: " + doubleTrash);
             }
             else
             {
-                int trash = in.nextInt();
-                in.nextLine();
-                System.out.println("You entered an incorrect value, '" + trash + "'. You must enter a number between 1 and 10 inclusive.");
+                String trash = in.nextLine();
+                System.out.println("You entered an incorrect value: '" + trash + "'");
             }
-        }
-        else if (in.hasNextDouble())
-        {
-            double doubleTrash = in.nextDouble();
-            in.nextLine();
-            System.out.println("You entered an incorrect value: " + doubleTrash);
-        }
-        else
-        {
-            String trash = in.nextLine();
-            in.nextLine();
-            System.out.println("You entered an incorrect value: " + trash);
-        }
-
-    } while(!done);
+        } while(!done);
     }
 }
